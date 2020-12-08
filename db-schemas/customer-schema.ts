e7f640dca8d6460f4fcc28c8c60aecc317264403
Abhishree Shetty <abshett@microsoft.com>
2020-12-08 18:50:08 +0530

    adding routing and hosting

---
+++ db-schemas/customer-schema.ts
@@ -0,0 +1,13 @@
+const CustomerSchema = {
+    customerId: Number,
+    customerName: String,
+    address: String,
+    email: String,
+    phoneNumber: String,
+    creditLimit: Number,
+    activeStatus: Boolean,
+    customerType: String,
+    remarks: String
+};
+
+export { CustomerSchema }
